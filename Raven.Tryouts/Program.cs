5713444eab7e2a3b174b9a7f9a061eef73f6aa19
Stein Hoem <stein.hoem@gmail.com>
2013-02-22 10:41:18 +0100

    Benchmarking

--- Raven.Tryouts/Program.cs
+++ Raven.Tryouts/Program.cs
@@ -1,4 +1,6 @@
-﻿namespace Raven.Tryouts
+﻿using System.Diagnostics;
+
+namespace Raven.Tryouts
 {
     #region
 
@@ -15,16 +17,16 @@
     {
         #region Methods
 
-        private int forthCounter = 0;
+        private static int forthCounter = 0;
         private static string GetRandomForthTag()
         {
-            secondCounter++;
-            if (secondCounter > 21)
+            forthCounter++;
+            if (forthCounter > 21)
             {
-                secondCounter = 0;
+                forthCounter = 0;
             }
 
-            return secondCounter.ToString() + "Number";
+            return forthCounter.ToString() + "Number";
         }
 
         private static int secondCounter = 0;
@@ -92,72 +94,112 @@
             {
                 documentStore.Initialize();
                 IndexCreation.CreateIndexes(typeof(DummyIndex).Assembly, documentStore);
-
+                //Console.ReadKey();
+                var stopwatch = new Stopwatch();
+                stopwatch.Start();
+                var lastTimeSpan = new TimeSpan();
                 var id = 1;
-                for (var o = 0; o < 10; o++)
+                for (var o = 0; o < 100; o++)
                 {
                     using (var bulkInsertOperation = documentStore.BulkInsert())
                     {
-                        for (var i = 0; i < 20000; i++)
+                        for (var i = 0; i <20000; i++)
                         {
                             var dummyObject = new DummyObject { Id = id, Tags = GetTags() };
                             bulkInsertOperation.Store(dummyObject);
                             id++;
                         }
                     }
+
+                    // wait for nonstale
+                    
+                    var doit = true;
+                    while (doit)
+                    {
+                        using (var openSessionI = documentStore.OpenSession())
+                        {
+                            try
+                            {
+                                var nonstaleresult =
+                                    openSessionI.Query<DummyIndex.Result, DummyIndex>()
+                                                .Customize(x => x.WaitForNonStaleResultsAsOfNow())
+                                                .Take(1)
+                                                .ToList();
+                                if (nonstaleresult.Count > 0)
+                                {
+                                    doit = false;
+                                }
+
+                            }
+                            catch (Exception)
+                            {
+                                System.Threading.Thread.Sleep(10000);
+                            }
+                        }
+                    }
+
+                    using (var openSession = documentStore.OpenSession())
+                    {
+                        var timeSpan = stopwatch.Elapsed;
+                        var diff = timeSpan - lastTimeSpan;
+                        lastTimeSpan = timeSpan;
+                        openSession.Store(new PerfStats() { NonStaleCount = id - 1, Timer = timeSpan, Difference = diff });
+                        openSession.SaveChanges();
+                    }
                 }
+                
+                stopwatch.Stop();
 
-                Console.WriteLine("Wait for nonstale indexes: press any key");
-                Console.ReadKey();
+                int sumOfAllTheSameTag;
+                using (var session = documentStore.OpenSession())
+                {
+                    Console.WriteLine("Wait for nonstale indexes: press any key");
+                    Console.ReadKey();
 
-                var session = documentStore.OpenSession();
+                
 
-                var sumOfAllTags =
-                    session.Query<DummyIndex.Result, DummyIndex>()
-                           .Customize(x => x.WaitForNonStaleResultsAsOfNow())
-                           .Take(1000)
-                           .ToList()
-                           .Sum(x => x.Count);
-                Console.WriteLine(
-                    "sum of all the tags should equal all document * 4 : " + (id - 1) + " documents, " + sumOfAllTags
-                    + " tags....");
+                    var sumOfAllTags =
+                        session.Query<DummyIndex.Result, DummyIndex>()
+                               .Customize(x => x.WaitForNonStaleResultsAsOfNow())
+                               .Take(1000)
+                               .ToList()
+                               .Sum(x => x.Count);
+                    Console.WriteLine(
+                        "sum of all the tags should equal all document * 4 : " + (id - 1) + " documents, " + sumOfAllTags
+                        + " tags....");
 
-                var sumOfSecondTag =
-                    session.Query<DummyIndex.Result, DummyIndex>()
-                           .Where(x => x.Type == "SecondTagWithFourValues")
-                           .Take(1000)
-                           .ToList()
-                           .Sum(x => x.Count);
-                Console.WriteLine(
-                    "sum of secondtag should equal all document: " + (id - 1) + " documents, " + sumOfSecondTag
-                    + " SecondTagWithFourValues....");
+                    var sumOfSecondTag =
+                        session.Query<DummyIndex.Result, DummyIndex>()
+                               .Where(x => x.Type == "SecondTagWithFourValues")
+                               .Take(1000)
+                               .ToList()
+                               .Sum(x => x.Count);
+                    Console.WriteLine(
+                        "sum of secondtag should equal all document: " + (id - 1) + " documents, " + sumOfSecondTag
+                        + " SecondTagWithFourValues....");
 
-                var sumOfThirdTag =
-                    session.Query<DummyIndex.Result, DummyIndex>()
-                           .Where(x => x.Type == "ThirdWithTwoValues")
-                           .Take(1000)
-                           .ToList()
-                           .Sum(x => x.Count);
-                Console.WriteLine(
-                    "sum of thirdtag should equal all document: " + (id - 1) + " documents, " + sumOfThirdTag
-                    + " ThirdWithTwoValues....");
+                    var sumOfThirdTag =
+                        session.Query<DummyIndex.Result, DummyIndex>()
+                               .Where(x => x.Type == "ThirdWithTwoValues")
+                               .Take(1000)
+                               .ToList()
+                               .Sum(x => x.Count);
+                    Console.WriteLine(
+                        "sum of thirdtag should equal all document: " + (id - 1) + " documents, " + sumOfThirdTag
+                        + " ThirdWithTwoValues....");
 
-                var sumOfForthTag =
-                    session.Query<DummyIndex.Result, DummyIndex>()
-                           .Where(x => x.Type == "SpeciesGroupId")
-                           .Take(1000)
-                           .ToList()
-                           .Sum(x => x.Count);
-                Console.WriteLine(
-                    "sum of forthtag should equal all document: " + (id - 1) + " documents, " + sumOfForthTag
-                    + " SpeciesGroupId....");
+                    var sumOfForthTag =
+                        session.Query<DummyIndex.Result, DummyIndex>()
+                               .Where(x => x.Type == "SpeciesGroupId")
+                               .Take(1000)
+                               .ToList()
+                               .Sum(x => x.Count);
+                    Console.WriteLine(
+                        "sum of forthtag should equal all document: " + (id - 1) + " documents, " + sumOfForthTag
+                        + " SpeciesGroupId....");
 
-                var sumOfAllTheSameTag =
-                    session.Query<DummyIndex.Result, DummyIndex>()
-                           .Where(x => x.Type == "AllTheSame")
-                           .Take(1000)
-                           .ToList()
-                           .Sum(x => x.Count);
+                    sumOfAllTheSameTag = session.Query<DummyIndex.Result, DummyIndex>().Where(x => x.Type == "AllTheSame").Take(1000).ToList().Sum(x => x.Count);
+                }
                 Console.WriteLine(
                     "sum of allthesametag should equal all document: " + (id - 1) + " documents, " + sumOfAllTheSameTag
                     + " allthesametags....");
@@ -167,6 +209,13 @@
         }
 
         #endregion
+    }
+
+    public class PerfStats
+    {
+        public int NonStaleCount { get; set; }
+        public TimeSpan Timer { get; set; }
+        public TimeSpan Difference { get; set; }
     }
 
     public class DummyIndex : AbstractIndexCreationTask<DummyObject, DummyIndex.Result>
