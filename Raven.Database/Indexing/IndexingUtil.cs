37006a2bd875a6f373a070477498196bfde3e2ef
Stein Hoem <stein.hoem@gmail.com>
2013-03-17 18:45:18 +0100

    Custom bucketGenerator

--- Raven.Database/Indexing/IndexingUtil.cs
+++ Raven.Database/Indexing/IndexingUtil.cs
@@ -10,17 +10,38 @@ using System.Text;
 
 namespace Raven.Database.Indexing
 {
-	public class IndexingUtil
-	{
-		public static int AbsStableInvariantIgnoreCaseStringHash(string s)
-		{
-			return Math.Abs(s.Aggregate(11, (current, ch) => (char.ToUpperInvariant(ch).GetHashCode() * 397) ^ current));
-		}
+    public class IndexingUtil
+    {
+        public static int StableInvariantIgnoreCaseStringHash(string s)
+        {
+            return s.Aggregate(11, (current, ch) => (char.ToUpperInvariant(ch).GetHashCode() * 397) ^ current);
+        }
 
-		public static int MapBucket(string docId)
-		{
-			var hash = AbsStableInvariantIgnoreCaseStringHash(docId);
-			return hash % (1024 * 1024);
-		}
-	}
+        public static int MapBucket(string docId)
+        {
+            int digitsHash = 0, nonDigitsHash = 11;
+            int nonDigitsCount = 0, digitsCount = 0;
+
+            for (int i = docId.Length - 1; i >= 0; i--)
+            {
+                var ch = docId[i];
+                if (char.IsDigit(ch))
+                {
+                    digitsHash = (ch - '0') * (int)Math.Pow(10, digitsCount) + digitsHash;
+                    digitsCount++;
+
+                }
+                else
+                {
+                    nonDigitsCount++;
+                    if (nonDigitsCount == 3) // we are on the third char, so we have more than 2 chars
+                        nonDigitsHash = 11; // will only hash the len -2 chars, this way we have a more common prefix
+                    nonDigitsHash = (ch * 397) ^ nonDigitsHash;
+                }
+            }
+            digitsHash /= 1024; // will force concentration of more items under the normal case to the same bucket
+
+            return Math.Abs(digitsHash) + Math.Abs(nonDigitsHash) % (1024 * 1024);
+        }
+    }
 }
